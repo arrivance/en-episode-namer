fb9fdf9d47de72184f5d36fe7754a50559e4543f
arrivance <arrivance@gmail.com>
2015-08-12 21:44:26 +0100

    Changelog:
    - refactored the code to be more efficient

--- ep_namer.py
+++ ep_namer.py
@@ -8,6 +8,37 @@ def debug_print(text):
 
     return None
 
+def file_renamer(file_item, ep_no, file_extension):
+    print("Original: " + file_item)
+    episode = t[title][season][ep_no]
+    episodename = episode['episodename']
+
+    episodename = episodename.replace('/', '')
+    episodename = episodename.replace(':', '')
+    
+    if len(str(ep_no)) != 2:
+        ins_ep_no = "0" + str(ep_no)
+    else:
+        ins_ep_no = str(ep_no)
+
+
+    if file_extension == None:
+        filename, file_extension = os.path.splitext(file_item)
+    filename = title + " - " + "[" + str(season) + "x" + ins_ep_no + "]" + " - " + episodename  + file_extension
+    ep_no += 1
+    print("Changed:  " + filename)
+
+    if safe == True:
+        verify = input("Are you sure? y/n | ")
+        if verify != "y":
+            sys.exit()
+        else: 
+            os.rename(file_item, filename)
+    else:
+        os.rename(file_item, filename)
+    print("Changed filename")
+    print("---")
+
 title = str(input("What is the name of the show?: "))
 season = int(input("What season?: "))
 file_format = str(input("What file format(s) are the files stored in? (seperate different formats with a comma): "))
@@ -43,7 +74,7 @@ for x in file_list:
     elif subtitles == "y" and file_extension == ".srt":
         sub_list.append(x)
     else:
-        debug_print("removed", filename, "fileext", file_extension)
+        debug_print("removed " + filename+ " fileext " + file_extension)
 
 file_list = sorted(file_list_temp)
 del(file_list_temp)
@@ -51,63 +82,11 @@ del(file_list_temp)
 debug_print(file_list)
 
 for file_item in file_list: 
-    print("Original: " + file_item)
-    episode = t[title][season][ep_no]
-    episodename = episode['episodename']
-
-    episodename = episodename.replace('/', '')
-    episodename = episodename.replace(':', '')
-    
-    if len(str(ep_no)) != 2:
-        ins_ep_no = "0" + str(ep_no)
-    else:
-        ins_ep_no = str(ep_no)
-
-    filename, file_extension = os.path.splitext(file_item)
-    filename = title + " - " + "[" + str(season) + "x" + ins_ep_no + "]" + " - " + episodename  + file_extension
-    ep_no += 1
-    print("Changed:  " + filename)
-
-    if safe == True:
-        verify = input("Are you sure? y/n | ")
-        if verify != "y":
-            sys.exit()
-        else: 
-            os.rename(file_item, filename)
-    else:
-        os.rename(file_item, filename)
-    print("Changed filename")
-    print("---")
+    file_renamer(file_item, ep_no, None)
 
 print("Proceeding to subtitles...")
 
 ep_no = 1
 if subtitles == "y":
     for file_item in sub_list: 
-        print("Original: " + file_item)
-        episode = t[title][season][ep_no]
-        episodename = episode['episodename']
-
-        episodename = episodename.replace('/', '')
-        episodename = episodename.replace(':', '')
-        
-        if len(str(ep_no)) != 2:
-            ins_ep_no = "0" + str(ep_no)
-        else:
-            ins_ep_no = str(ep_no)
-
-        filename, file_extension = os.path.splitext(file_item)
-        filename = title + " - " + "[" + str(season) + "x" + ins_ep_no + "]" + " - " + episodename  + ".srt"
-        ep_no += 1
-        print("Changed:  " + filename)
-
-        if safe == True:
-            verify = input("Are you sure? y/n | ")
-            if verify != "y":
-                sys.exit()
-            else: 
-                os.rename(file_item, filename)
-        else:
-            os.rename(file_item, filename)
-        print("Changed filename")
-        print("---")
+        file_renamer(file_item, ep_no, ".srt")
